936c1e5f845801c20c4be5ae1e694616b39144ed
matfigueroac <134018353+matfigueroac@users.noreply.github.com>
2024-05-20 10:33:31 -0400

    Added templates and updated main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, send_file
 import os
 import pandas as pd
-from scraper import run_scraper  # Asegúrate de tener esta importación correcta
+from scraper import run_scraper
 
 app = Flask(__name__)
 
@@ -15,12 +15,10 @@ def scrape():
     password = request.form['password']
     urls = request.form['urls'].splitlines()
     
-    # Llama a la función de scraping
     run_scraper(username, password, urls)
     
-    # Devuelve el archivo CSV generado
     return send_file('LinkedIn_Contacts_Info.csv', as_attachment=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
